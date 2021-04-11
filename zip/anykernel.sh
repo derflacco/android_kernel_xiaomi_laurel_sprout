5e0d5b7662f072aae6c9f31cbefd78850821d161
derflacco <paul89rulez@gmail.com>
2021-04-11 18:53:29 +0200

    fenix: Xenomorph

--- zip/anykernel.sh
+++ zip/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Fenix-Kernel_Q by derflacco
+kernel.string=Fenix-Kernel_R by derflacco
 do.devicecheck=0
 do.modules=0
 do.systemless=1
